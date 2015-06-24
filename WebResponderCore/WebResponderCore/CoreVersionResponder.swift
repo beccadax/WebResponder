c8c7287dc0500a5d331b42259d5da9370f9f5dfa
Brent Royal-Gordon <brent@architechies.com>
2015-06-24 04:50:15 -0700

    Add a responder for testing

---
+++ WebResponderCore/WebResponderCore/CoreVersionResponder.swift
@@ -0,0 +1,29 @@
+//
+//  CoreVersionResponder.swift
+//  WebResponderCore
+//
+//  Created by Brent Royal-Gordon on 6/24/15.
+//  Copyright © 2015 Groundbreaking Software. All rights reserved.
+//
+
+import Foundation
+
+class CoreVersionResponder: WebResponderType {
+    func respond(response: HTTPResponseType, toRequest request: HTTPRequestType) {
+        var unameResult = utsname()
+        uname(&unameResult)
+        let platform = withUnsafePointer(&unameResult.version) { tuplePointer in
+            String.fromCString(UnsafePointer<Int8>(tuplePointer))!
+        } 
+
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let version = bundle.objectForInfoDictionaryKey("CFBundleShortVersionString") as! String
+        let copyright = bundle.objectForInfoDictionaryKey("NSHumanReadableCopyright") as! String
+        
+        response.status = .OK
+        response.headers["Content-Type"] = ["text/html; charset=UTF-8"]
+        response.body = HTTPBody(string: "<!DOCTYPE html><html><head><title>WebResponderCore</title><style>html { font-family: sans-serif; margin: 1em 25%; background-color: #eee } body { border: 1px solid #ddd; background-color: white; font-size: 1.2em/1.5em } dt { font-weight: bold } dd { margin: 0; padding: 0; margin-bottom: 0.5em; }</style></head><body><h1>WebResponderCore</h1><dl><dt>Version</dt><dd>\(version)</dd><dt>Platform</dt><dd>\(platform)</dd><dt>Copyright</dt><dd>\(copyright)</dd></dl></body></html>", codec: UTF8.self)
+        
+        response.respond()
+    }
+}
