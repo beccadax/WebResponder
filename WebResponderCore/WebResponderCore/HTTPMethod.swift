30851ad57cdc29559108ea6d3eb33a6d2ae85290
Brent Royal-Gordon <brent@architechies.com>
2015-07-01 20:12:40 -0700

    Document HTTPStatus and HTTPMethod

--- WebResponderCore/WebResponderCore/HTTPMethod.swift
+++ WebResponderCore/WebResponderCore/HTTPMethod.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2015 Groundbreaking Software. All rights reserved.
 //
 
+/// Represents an HTTP method. Currently incomplete and temporary.
 public enum HTTPMethod: String {
     case GET = "GET"
     case POST = "POST"
--- WebResponderCore/WebResponderCore/HTTPStatus.swift
+++ WebResponderCore/WebResponderCore/HTTPStatus.swift
@@ -6,14 +6,26 @@
 //  Copyright © 2015 Groundbreaking Software. All rights reserved.
 //
 
+/// Represents the status of an HTTP response.
+/// 
+/// `HTTPStatus` includes an extensive collection of constants representing all 
+/// IANA-assigned HTTP statuses. It also supports `IntegerLiteralConvertible`, so
+/// you can type a number like `200` anywhere in your code that requires an 
+/// `HTTPStatus`.
 public struct HTTPStatus {
+    /// The status code of this status, e.g. 200 or 404.
+    /// 
+    /// - Precondition: does not have to be allocated by IANA, but must be between
+    ///    100 and 599, i.e. in one of the known classes of status codes.
     public var code: Int
     
+    /// Constructs an `HTTPStatus` from its code.
     public init(code: Int) {
         precondition(isValid(code), "Invalid status code \(code)")
         self.code = code
     }
     
+    /// Represents a class of statuses, e.g. 3xx Redirection or 5xx Server Error.
     public enum Classification: Int {
         case Informational = 1
         case Success = 2
@@ -22,10 +34,14 @@ public struct HTTPStatus {
         case ServerError = 5
     }
     
+    /// The class of this status. If you don't recognize a specific status, you can 
+    /// use this to figure out what general sort of condition it represents.
     public var classification: Classification {
         return Classification(rawValue: code / 100)!
     }
     
+    /// The plain-text message used to explain this status code, e.g. `"OK"` or 
+    /// `"Not Found"`.
     public var message: String {
         return HTTPStatus.messages[self] ?? "Unassigned"
     }
@@ -60,6 +76,7 @@ extension HTTPStatus: Hashable {
     }
 }
 
+/// Two `HTTPStatus`es are equal if their `code`s are equal.
 public func == (lhs: HTTPStatus, rhs: HTTPStatus) -> Bool {
     return lhs.code == rhs.code
 }
