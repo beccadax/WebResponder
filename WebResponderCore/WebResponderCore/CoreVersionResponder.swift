d3a2b8f99059af644faeea5ba0c5b2629a2f9619
Brent Royal-Gordon <brent@architechies.com>
2015-07-02 03:55:15 -0700

    Improve CoreVersionResponder styles

--- WebResponderCore/WebResponderCore/CoreVersionResponder.swift
+++ WebResponderCore/WebResponderCore/CoreVersionResponder.swift
@@ -22,7 +22,7 @@ public class CoreVersionResponder: WebResponderType {
         
         response.setStatus(.OK)
         response.setHeaders(["Content-Type": ["text/html; charset=UTF-8"]])
-        response.setBody(HTTPBody(string: "<!DOCTYPE html><html><head><title>WebResponderCore</title><style>html { font-family: sans-serif; margin: 1em 25%; background-color: #eee } body { border: 1px solid #ddd; background-color: white; font-size: 1.2em/1.5em } dt { font-weight: bold } dd { margin: 0; padding: 0; margin-bottom: 0.5em; }</style></head><body><h1>WebResponderCore</h1><dl><dt>Version</dt><dd>\(version)</dd><dt>Platform</dt><dd>\(platform)</dd></dl></body></html>", codec: UTF8.self))
+        response.setBody(HTTPBody(string: "<!DOCTYPE html><html><head><title>WebResponderCore</title><style>html { font-family: sans-serif; margin: 1ex 25%; background-color: #eee } body { padding: 1em; border: 1px solid #ddd; background-color: white; font-size: 1.2em/1.5em } h1 { margin-top: 0 } dl { margin: 0 } dt { font-weight: bold } dd { margin: 0; padding: 0; margin-bottom: 0.5em; }</style></head><body><h1>WebResponderCore</h1><dl><dt>Version</dt><dd>\(version)</dd><dt>Platform</dt><dd>\(platform)</dd></dl></body></html>", codec: UTF8.self))
         
         response.respond()
     }
