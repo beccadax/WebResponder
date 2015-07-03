da284e0a4eacdb19bd31d8c26a18396cdc212737
Brent Royal-Gordon <brent@architechies.com>
2015-07-03 03:53:40 -0700

    Escape target in CoreVersionResponder
    Since this is user-provided.

--- WebResponderCore/WebResponderCore/CoreVersionResponder.swift
+++ WebResponderCore/WebResponderCore/CoreVersionResponder.swift
@@ -22,8 +22,13 @@ public class CoreVersionResponder: WebResponderType {
         
         response.setStatus(.OK)
         response.setHeaders(["Content-Type": ["text/html; charset=UTF-8"]])
-        response.setBody(HTTPBody(string: "<!DOCTYPE html><html><head><title>WebResponderCore</title><style>html { font-family: sans-serif; margin: 1ex 25%; background-color: #eee } body { padding: 1em; border: 1px solid #ddd; background-color: white; font-size: 1.2em/1.5em } h1 { margin-top: 0 } dl { margin: 0 } dt { font-weight: bold } dd { margin: 0; padding: 0; margin-bottom: 0.5em; }</style></head><body><h1>WebResponderCore</h1><dl><dt>Version</dt><dd>\(version)</dd><dt>Platform</dt><dd>\(platform)</dd><dt>Request Target</dt><dd>\(request.target)</dd></dl></body></html>", codec: UTF8.self))
+        response.setBody(HTTPBody(string: "<!DOCTYPE html><html><head><title>WebResponderCore</title><style>html { font-family: sans-serif; margin: 1ex 25%; background-color: #eee } body { padding: 1em; border: 1px solid #ddd; background-color: white; font-size: 1.2em/1.5em } h1 { margin-top: 0 } dl { margin: 0 } dt { font-weight: bold } dd { margin: 0; padding: 0; margin-bottom: 0.5em; }</style></head><body><h1>WebResponderCore</h1><dl><dt>Version</dt><dd>\(version)</dd><dt>Platform</dt><dd>\(platform)</dd><dt>Request Target</dt><dd>\(aggressivelyEscapeHTML(request.target))</dd></dl></body></html>", codec: UTF8.self))
         
         response.respond()
     }
+}
+
+private func aggressivelyEscapeHTML(string: String) -> String {
+    // Let's not try to figure out which characters are safe; just escape everything.
+    return lazy(string.unicodeScalars).map { $0.value }.map { "&#\($0);" }.reduce("", combine: +)
 }
