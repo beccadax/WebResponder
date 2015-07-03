84f3d69d7593007e13605bd47f310794b59617a6
Brent Royal-Gordon <brent@architechies.com>
2015-07-03 01:49:19 -0700

    Fix some missed renaming

--- WebResponderGCDWebServer/WebResponderGCDWebServer/GCDWebServerAdapterResponse.swift
+++ WebResponderGCDWebServer/WebResponderGCDWebServer/GCDWebServerAdapterResponse.swift
@@ -1,6 +1,6 @@
 //
 //  GCDWebServerAdapterResponse.swift
-//  GCDWebServerWebResponderAdapter
+//  WebResponderGCDWebServer
 //
 //  Created by Brent Royal-Gordon on 7/2/15.
 //  Copyright © 2015 Groundbreaking Software. All rights reserved.
---
+++ WebResponderGCDWebServer/WebResponderGCDWebServer/WebResponderGCDWebServer.h
@@ -0,0 +1,19 @@
+//
+//  WebResponderGCDWebServer.h
+//  WebResponderGCDWebServer
+//
+//  Created by Brent Royal-Gordon on 7/1/15.
+//  Copyright © 2015 Groundbreaking Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GCDWebServerWebResponderAdapter.
+FOUNDATION_EXPORT double WebResponderGCDWebServerVersionNumber;
+
+//! Project version string for GCDWebServerWebResponderAdapter.
+FOUNDATION_EXPORT const unsigned char WebResponderGCDWebServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebResponderGCDWebServer/PublicHeader.h>
+
+
